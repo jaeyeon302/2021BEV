ac48b6e2cc7819daadbddef2f5f5ff6e18c3b16d
Jaeyeon <parkjani2@gmail.com>
2021-03-12 18:37:22 +0900

    organized DQ PI control logic

--- DSP_Programming/controller.c
+++ DSP_Programming/controller.c
@@ -25,6 +25,7 @@ Current_Controller CCd,CCq;
 /**********************************/
 // Functions for DQ PI control with SVPWM
 void Init_CC(Current_Controller* cc, float32 v_sat ){
+    cc->I_ref = 0.0;
     cc->V_sat = v_sat;
     cc->V_anti = 0.0;
     cc->V_int = 0.0;
@@ -45,9 +46,7 @@ void dq2abc(float32 d, float32 q, float32 angle_rad, float32* a, float32* b ,flo
     *b = cos(angle_rad - 2*PI/3)*d - sin(angle_rad - 2*PI/3)*q;
     *c = cos(angle_rad + 2*PI/3)*d - sin(angle_rad + 2*PI/3)*q;
 }
-
-
-float32 SVPMW_offset_voltage(float32 phase_a_v,float32 phase_b_v, float32 phase_c_v){
+float32 SVPWM_offset_voltage(float32 phase_a_v,float32 phase_b_v, float32 phase_c_v){
     float32 vmax = 0;
     float32 vmin = 0;
     if (phase_a_v > phase_b_v){
@@ -70,6 +69,7 @@ float32 SVPMW_offset_voltage(float32 phase_a_v,float32 phase_b_v, float32 phase_
 void control_DQ(float32* phase_current,
                 Current_Controller* ccId, Current_Controller* ccIq,
                 float32 angle, float32 angular_speed,
+                float32 max_v_pole,
                 float32* duty_out){
     // angle: rad
     // angle_speed : rad/sec
@@ -95,14 +95,17 @@ void control_DQ(float32* phase_current,
     ccIq->V_int += ccIq->ki*(I_err - ccIq->ka*ccIq->V_anti);
     ccIq->V_fb = ccIq->V_int + ccIq->kp*I_err;
 
-    ccId->V_ref = ccId->V_fb + (-Ls_DEFAULT*angular_speed*Iq_fb);
-    ccIq->V_ref = ccIq->V_fb + (angular_speed*Ls_DEFAULT*Id_fb + angular_speed*flux_DEFAULT);
+    ccId->V_ref = ccId->V_fb + (-Ls_DEFAULT*angular_speed*Iq_fb); // add Vd feeadforward
+    ccIq->V_ref = ccIq->V_fb + (angular_speed*Ls_DEFAULT*Id_fb + angular_speed*flux_DEFAULT); // Vq feedforward
 
 
-    vdc = BAT_DEFAULT_VOLTAGE;
-    voffset_pole = vdc/2;
+
     dq2abc(ccId->V_ref, ccIq->V_ref, angle, &va, &vb, &vc);
-    vsn = SVPMW_offset_voltage(va,vb,vc);
+    vsn = SVPWM_offset_voltage(va,vb,vc);
+
+    vdc = max_v_pole;
+    voffset_pole = vdc/2;
+
     vsn += voffset_pole;
 
     va += vsn;
@@ -118,9 +121,19 @@ void control_DQ(float32* phase_current,
     duty_out[phaseW] = vc/vdc;
 
 }
-void control(){
+void control_sinusoidal_BEMF(){
+    hall_sensor_update();
     float32 duty[3]={0,0,0};
-    float32 angle, angular_speed;
+    float32 angle, angular_speed; // TODO update needed
+
+    CCd.I_ref = 0.0;
+    CCq.I_ref = throttle_result;
+
+    // calculate Iabc->DQ->PI->DQ->Vabc
+    control_DQ(phase_current_result,
+               &CCd, &CCq, angle, angular_speed,
+               BAT_DEFAULT_VOLTAGE,
+               duty);
 
 
     epwm1_set_duty(duty[phaseU], 1-duty[phaseU]);
@@ -132,7 +145,6 @@ void control(){
 
 void control_state_update(enum ADC_RESULT_TYPE type, float32 adc_result_voltage){
     float32 current = (adc_result_voltage - CURRENT_SENSOR_VOLTAGE_OFFSET_FOR_ZERO ) * ADC_Voltage2Current; //[Ampere]
-    //float32 current = adc_result_voltage/100.0; // TODO FOR THE TEST. REMOVE THIS LINE TO OPERATE MOTOR
 
     switch(type){
     case ADCcurrentPhaseU:
@@ -150,7 +162,6 @@ void control_state_update(enum ADC_RESULT_TYPE type, float32 adc_result_voltage)
     case ADCthrottle:
         adc_result_flag |= FLAG_ADC_THROTTLE_SAMPLED;
         //FOR TEST
-        //throttle_result = 0.015/4; //TODO FOR THE TEST. REMOVE THIS LINE TO OPERATE THE MOTOR
         throttle_result = adc_result_voltage*ADC_Voltage2Current*CURRENT_LIMIT_SCALE;
         break;
     case ADCbatteryLevel:
@@ -165,13 +176,8 @@ void control_state_update(enum ADC_RESULT_TYPE type, float32 adc_result_voltage)
         controlCycleCount++;
         /* 제어 코드는 여기서 호출되어야 한다 */
 
-        //control_sinusoidal_BEMF();  // Q CURRENT CONTROL - PMSM
-        //control_trapezoidal_BEMF(); // 3 CURRENT CONTROLLER 3 PHASE DC
-        //control_trapezoidal_BEMF0(); // 1 CURRENT CCONTROLLER 3 PHASE DC
-        //test_just_run();//
-        //test_3phase_pole_voltage(0.25); // test Pole Voltage with duty
         //throttle_result = 3; //[A]
-        //control_sinusoidal_BEMF();
+        control_sinusoidal_BEMF();
         adc_result_flag = 0x00; //CLEAR FLAG for next sampling
     }
 }
@@ -190,14 +196,18 @@ void Ready_controller(){
     // 만일 그렇지 않으면 ADC, ECAP이 initialize 되는 동안
     // start_controller에 의해 ePWM이 시작되고 ePWM cycle (2~3개)가 그냥 지나가버린다
     // ePWM -> ADC 호출 -> 제어함수 호출 구조이기 때문에, ADC를 ePWM보다 먼저 설정을 완료해야한다.
+    Init_CC(&CCd, 40); // Vd_sat 40
+    Init_CC(&CCq, 40); // Vq_sat 40
 
     Init_3current_ADC( &control_state_update );
     Init_misc_ADC();
     Init_3phase_ePWM();
+    Init_hall_sensor(23, 100000);
 }
 
 void Start_controller(){
     Start_3phase_ePWM();
     Start_3current_ADC();
+    Start_hall_sensor();
 
 }
